0e27958301c67f92253a9d5a76dbdee0db80518e
tranduy110398 <trandinhduy110398@gmail.com>
2024-12-04 10:48:43 +0700

    Initial commit from Create Next App

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
-const dashboardWrapper = ({children} : {children: React.ReactNode}) => {
-return (
+// Navbar always at the layout
+const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-    Sidebar
-    <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}><Navbar/>{children}</main>
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <Navbar />
+        {children}
+      </main>
     </div>
-)
-}
+  );
+};
 
-export default dashboardWrapper
+export default dashboardWrapper;
